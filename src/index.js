784c7b8f21bcaa7a40888805342f1462b2b1914f
pandapo777 <85247214+pandapo777@users.noreply.github.com>
2023-11-20 20:56:11 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import config from './config.js';
 const app = express();
 let server = null;
 
-consol.log ('Hello, OTUS!')
+console.log ('Hello, OTUS!')
 
 app.get('/hello/:name', (req, res) => {
   const { name } = req.params;
