a57c3aef3114c98944aa18a64827963f0ac3ba1c
lazerlemon <63816623+lazerlemon@users.noreply.github.com>
2024-02-11 21:36:17 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import config from './config.js';
 const app = express();
 let server = null;
 
-console.log ('hello,test!')
-
 app.get('/hello/:name', (req, res) => {
   const { name } = req.params;
 
