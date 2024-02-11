01aa8f2b87ac3a0dbb6bf4f397a6b6a9c344c9c0
lazerlemon <63816623+lazerlemon@users.noreply.github.com>
2024-02-11 21:44:08 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import config from './config.js';
 const app = express();
 let server = null;
 
+console.log('added console.log again')
+
 app.get('/hello/:name', (req, res) => {
   const { name } = req.params;
 
