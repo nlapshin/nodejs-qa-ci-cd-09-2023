0c346404fdcd0a0ea8a62ab412516681da5c0bf9
lazerlemon <poloska-87@mail.ru>
2024-02-11 21:06:17 +0300

    test commit

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import config from './config.js';
 const app = express();
 let server = null;
 
-console.log ('hello,test')
+console.log ('hello,test!')
 
 app.get('/hello/:name', (req, res) => {
   const { name } = req.params;
